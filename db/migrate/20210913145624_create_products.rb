ccedaf47ee21a8ad8b9a245e7aa814efbdeac4d7
William Badger <wkbadger@gmail.com>
2021-09-13 10:03:47 -0500

    models

---
+++ db/migrate/20210913145624_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :name,
+      t.integer, :price
+      t.string :image_url,
+      t.integer :likes
+      t.integer :brand_id
+
+      t.timestamps
+    end
+  end
+end
